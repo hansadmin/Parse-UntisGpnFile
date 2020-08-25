08bb992ddbe00146c04969124fb7f5ae92a47014
Hans Van Broeckhoven Admin <hansadmin@immalle.eu>
2020-08-25 20:27:38 +0200

    Fix encoding-issue for Tests

--- Tests.ps1
+++ Tests.ps1
@@ -1,212 +1,6 @@
 Remove-Module UntisGpnFileTools -ErrorAction SilentlyContinue
 Import-Module ./UntisGpnFileTools.psm1
 
-Describe 'UntisGpnFileTools module-scope stuff' {
-    AfterEach {
-        Close-UntisGpnFile
-    }
-    It "must open and close files correctly" {
-        { Get-UntisGpnFile } | Should -Throw
-        { Get-UntisGpnFileContent } | Should -Throw
-        Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN
-        Get-UntisGpnFile | Should -BeOfType System.IO.FileInfo
-        (Get-UntisGpnFile).Name | Should -Be 'be_gy1_Hantal.GPN'
-        Get-UntisGpnFileContent | Should -HaveCount 26640
-        Close-UntisGpnFile
-        { Get-UntisGpnFile } | Should -Throw
-        { Get-UntisGpnFileContent } | Should -Throw
-        { Get-UntisKlassen } | Should -Throw
-        { Get-UntisDocenten } | Should -Throw
-        { Get-UntisPeriodes } | Should -Throw
-        { Get-UntisActiviteiten } | Should -Throw
-        Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
-        (Get-UntisGpnFile).Name | Should -Be 'be_uv1_Nijverheidsschool.gpn'
-        Get-UntisGpnFileContent | Should -HaveCount 41482
-    }
-}
-
-Describe 'Get-UntisPeriodes' {
-    Context "Demo GPN file [be_gy1_Hantal.GPN]" {
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "geeft de juiste aantallen periodes terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes | Should -HaveCount 2
-        }
-        It "geeft afkorting en volledige naam eerste periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].afkorting | Should -Be "1ste semester"
-            $periodes[0].volledig | Should -Be "1ste semester"
-        }
-        It "geeft moederperiode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[1].moederperiode | Should -Be "1ste semester"
-        }
-        It "geef de juiste start- en einddata terug" {
-            [datetime]$expected_van = '2009-07-27'
-            [datetime]$expected_tem = '2010-07-02'
-            $periodes = Get-UntisPeriodes
-            $periodes[0].van | Should -BeOfType System.DateTime
-            $periodes[0].tem | Should -BeOfType System.DateTime
-            $periodes[0].van | Should -Be $expected_van
-            $periodes[0].tem | Should -Be $expected_tem
-        }
-        It "geeft de actieve periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].isActief | Should -Be $true
-            $periodes[1].isActief | Should -Be $false
-        }
-        It "geef de juiste nummer v.d. periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].nummer | Should -Be 1
-            $periodes[1].nummer | Should -Be 2
-        }
-    }
-    Context "Demo GPN file [be_uv1_Nijverheidsschool.gpn]" {
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "geeft de juiste aantallen periodes terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes | Should -HaveCount 3
-        }
-        It "geeft afkorting en volledige naam eerste periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].afkorting | Should -Be "Lesjaar"
-            $periodes[0].volledig | Should -Be "Heel lesjaar"
-        }
-        It "geeft moederperiode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[2].moederperiode | Should -Be "Lesjaar"
-        }
-        It "geef de juiste start- en einddata terug" {
-            [datetime]$expected_van = '2009-09-01'
-            [datetime]$expected_tem = '2010-06-30'
-            $periodes = Get-UntisPeriodes
-            $periodes[0].van | Should -BeOfType System.DateTime
-            $periodes[0].tem | Should -BeOfType System.DateTime
-            $periodes[0].van | Should -Be $expected_van
-            $periodes[0].tem | Should -Be $expected_tem
-        }
-        It "geeft de actieve periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].isActief | Should -Be $false
-            $periodes[1].isActief | Should -Be $false
-            $periodes[2].isActief | Should -Be $true
-        }
-        It "geef de juiste nummer v.d. periode terug" {
-            $periodes = Get-UntisPeriodes
-            $periodes[0].nummer | Should -Be 1
-            $periodes[1].nummer | Should -Be 2
-            $periodes[2].nummer | Should -Be 3
-        }
-    }
-}
-
-Describe 'Get-UntisKlassen' {
-    Context "Demo GPN file [be_gy1_Hantal.GPN] met handmatige data" {
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "moet alle 56 klassen returnen van alle periodes" {
-            $alle_klassen = Get-UntisKlassen
-            $alle_klassen.Count | Should -Be 56
-        }
-        It "moet als eerste klas 1BMA returnen" {
-            $eerste_klas = Get-UntisKlassen | Select-Object -First 1
-            $eerste_klas.afkorting | Should -Be '1BMA'
-            $eerste_klas.volledig | Should -Be 'Accountancy-Talen & Fiscaliteit'
-        }
-        It "moet 1 klas zonder volledige naam bevatten (STVMK)" {
-            $klas_zonder_volledige_naam = Get-UntisKlassen | Where-Object volledig -eq ""
-            $klas_zonder_volledige_naam.Count | Should -Be 1
-            $klas_zonder_volledige_naam.afkorting | Should -Be 'STVMK'
-        }
-        It "moet deeltekens correct parsen (2IMA)" {
-            $klas2IMA = Get-UntisKlassen | Where-Object afkorting -eq 2IMA
-            $klas2IMA.volledig | Should -Be "Immobiliën- en Verzekeringswezen"
-        }
-    }  
-}
-
-Describe 'Get-UntisDocenten' {
-    Context "Demo GPN file [be_gy1_Hantal.GPN]" {
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "moet alle 97 docenten return" {
-            $alle_docenten = Get-UntisDocenten
-            $alle_docenten.Count | Should -Be 97
-        }
-    }
-    Context "Demo GPN files vergelijken met export-data periode 1" {
-        $export_data_folders = @(
-            @{Path = "./demo-files/testdata_be_gy1_Hantal"},
-            @{Path = "./demo-files/testdata_be_uv1_Nijverheidsschool"}
-        )
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "moet elke docent returnen die ook in '<Path>' voor periode 1 aanwezig is" -TestCases $export_data_folders {
-            param($Path)
-            $expected_teachers = Get-UntisDataFromExport -Path $Path -Type Teachers
-            $alle_docenten = Get-UntisDocenten #-Periode 1 #TODO
-            $expected_teachers | Foreach-Object {
-                $alle_docenten.afkorting | Should -Contain $_.afkorting
-            }
-            $alle_docenten | Foreach-Object {
-                $expected_teachers.afkorting | Should -Contain $_.afkorting
-            }
-        }
-    }
-}
-
-Describe 'Get-UntisActiviteiten' {
-    Context "Demo GPN file [be_uv1_Nijverheidsschool.gpn]" {
-        BeforeAll {
-            Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
-        }
-        AfterAll {
-            Close-UntisGpnFile
-        }
-        It "geeft het juiste aantal activiteiten terug" {
-            $expected = Get-UntisActiviteiten
-            $expected | Should -HaveCount 104
-        }
-        It "geeft de juiste values voor alle properties van de eerste activiteit" {
-            $expected = Get-UntisActiviteiten | Select-Object -First 1
-            $expected.nummer | Should -Be 179
-            $expected.startdatum | Should -BeOfType System.DateTime
-            $expected.einddatum | Should -BeOfType System.DateTime
-            $expected.startdatum | Should -Be ([datetime]'2010-05-07')
-            $expected.einddatum | Should -Be ([datetime]'2010-05-07')
-            $expected.reden | Should -Be "SB"
-            $expected.tekst | Should -Be "Moskee Genk+Zutendaal"
-            $expected.startuur | Should -Be 1845
-            $expected.einduur | Should -Be 9
-            $expected.onderwerp | Should -Be ''
-            $expected.klassen | Should -Be @('B2BA','B2BB','B2BC')
-            $expected.leerkrachten | Should -Be @('VLOB','Van6','VGIE','Geen','va')
-            $expected.absent_ids | Should -Be @(2814,2813,2812,2815,2816,2817,2818,2819)
-        }
-    }
-}
 
 # Helper functions
 
@@ -235,7 +29,17 @@ function Get-UntisDataFromExport {
         'reductions' { 'GPU020.TXT' }
         'reduction_reasons' { 'GPU021.TXT' }
     }
-    $nr_of_columns = ((Get-Content "$Path/$filename" -Encoding 1252 | Select-Object -First 1).Split(',') | Measure-Object).Count
+
+    # Powershell (>= 6.0) ondersteunt GetEncoding(1252)
+    $iswinps = ($null, 'Desktop') -contains $PSVersionTable.PSEdition
+    if (!$iswinps) {
+        $encoding = [System.Text.Encoding]::GetEncoding(1252)
+    }
+    else {
+        $encoding = [Microsoft.PowerShell.Commands.FileSystemCmdletProviderEncoding]::Default
+    }
+
+    $nr_of_columns = ((Get-Content "$Path/$filename" -Encoding $encoding | Select-Object -First 1).Split(',') | Measure-Object).Count
     $colnames = 0..$nr_of_columns | Foreach-Object { "col$_"}
     switch($Type) {
         'timetables' {
@@ -447,6 +251,218 @@ function Get-UntisDataFromExport {
             break
          }
     }
-    $data = Get-Content "$Path/$filename" -Encoding 1252 | ConvertFrom-Csv -Header $colnames
+    $data = Get-Content "$Path/$filename" -Encoding $encoding | ConvertFrom-Csv -Header $colnames
     return $data
 }
+
+
+
+# Tests
+
+Describe 'UntisGpnFileTools module-scope stuff' {
+    AfterEach {
+        Close-UntisGpnFile
+    }
+    It "must open and close files correctly" {
+        { Get-UntisGpnFile } | Should -Throw
+        { Get-UntisGpnFileContent } | Should -Throw
+        Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN
+        Get-UntisGpnFile | Should -BeOfType System.IO.FileInfo
+        (Get-UntisGpnFile).Name | Should -Be 'be_gy1_Hantal.GPN'
+        Get-UntisGpnFileContent | Should -HaveCount 26640
+        Close-UntisGpnFile
+        { Get-UntisGpnFile } | Should -Throw
+        { Get-UntisGpnFileContent } | Should -Throw
+        { Get-UntisKlassen } | Should -Throw
+        { Get-UntisDocenten } | Should -Throw
+        { Get-UntisPeriodes } | Should -Throw
+        { Get-UntisActiviteiten } | Should -Throw
+        Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
+        (Get-UntisGpnFile).Name | Should -Be 'be_uv1_Nijverheidsschool.gpn'
+        Get-UntisGpnFileContent | Should -HaveCount 41482
+    }
+}
+
+Describe 'Get-UntisPeriodes' {
+    Context "Demo GPN file [be_gy1_Hantal.GPN]" {
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "geeft de juiste aantallen periodes terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes | Should -HaveCount 2
+        }
+        It "geeft afkorting en volledige naam eerste periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].afkorting | Should -Be "1ste semester"
+            $periodes[0].volledig | Should -Be "1ste semester"
+        }
+        It "geeft moederperiode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[1].moederperiode | Should -Be "1ste semester"
+        }
+        It "geef de juiste start- en einddata terug" {
+            [datetime]$expected_van = '2009-07-27'
+            [datetime]$expected_tem = '2010-07-02'
+            $periodes = Get-UntisPeriodes
+            $periodes[0].van | Should -BeOfType System.DateTime
+            $periodes[0].tem | Should -BeOfType System.DateTime
+            $periodes[0].van | Should -Be $expected_van
+            $periodes[0].tem | Should -Be $expected_tem
+        }
+        It "geeft de actieve periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].isActief | Should -Be $true
+            $periodes[1].isActief | Should -Be $false
+        }
+        It "geef de juiste nummer v.d. periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].nummer | Should -Be 1
+            $periodes[1].nummer | Should -Be 2
+        }
+    }
+    Context "Demo GPN file [be_uv1_Nijverheidsschool.gpn]" {
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "geeft de juiste aantallen periodes terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes | Should -HaveCount 3
+        }
+        It "geeft afkorting en volledige naam eerste periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].afkorting | Should -Be "Lesjaar"
+            $periodes[0].volledig | Should -Be "Heel lesjaar"
+        }
+        It "geeft moederperiode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[2].moederperiode | Should -Be "Lesjaar"
+        }
+        It "geef de juiste start- en einddata terug" {
+            [datetime]$expected_van = '2009-09-01'
+            [datetime]$expected_tem = '2010-06-30'
+            $periodes = Get-UntisPeriodes
+            $periodes[0].van | Should -BeOfType System.DateTime
+            $periodes[0].tem | Should -BeOfType System.DateTime
+            $periodes[0].van | Should -Be $expected_van
+            $periodes[0].tem | Should -Be $expected_tem
+        }
+        It "geeft de actieve periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].isActief | Should -Be $false
+            $periodes[1].isActief | Should -Be $false
+            $periodes[2].isActief | Should -Be $true
+        }
+        It "geef de juiste nummer v.d. periode terug" {
+            $periodes = Get-UntisPeriodes
+            $periodes[0].nummer | Should -Be 1
+            $periodes[1].nummer | Should -Be 2
+            $periodes[2].nummer | Should -Be 3
+        }
+    }
+}
+
+Describe 'Get-UntisKlassen' {
+    Context "Demo GPN file [be_gy1_Hantal.GPN] met handmatige data" {
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "moet alle 56 klassen returnen van alle periodes" {
+            $alle_klassen = Get-UntisKlassen
+            $alle_klassen.Count | Should -Be 56
+        }
+        It "moet als eerste klas 1BMA returnen" {
+            $eerste_klas = Get-UntisKlassen | Select-Object -First 1
+            $eerste_klas.afkorting | Should -Be '1BMA'
+            $eerste_klas.volledig | Should -Be 'Accountancy-Talen & Fiscaliteit'
+        }
+        It "moet 1 klas zonder volledige naam bevatten (STVMK)" {
+            $klas_zonder_volledige_naam = Get-UntisKlassen | Where-Object volledig -eq ""
+            $klas_zonder_volledige_naam.Count | Should -Be 1
+            $klas_zonder_volledige_naam.afkorting | Should -Be 'STVMK'
+        }
+        It "moet deeltekens correct parsen (2IMA)" {
+            $klas2IMA = Get-UntisKlassen | Where-Object afkorting -eq 2IMA
+            $klas2IMA.volledig | Should -Be "Immobiliën- en Verzekeringswezen"
+        }
+    }  
+}
+
+Describe 'Get-UntisDocenten' {
+    Context "Demo GPN file [be_gy1_Hantal.GPN]" {
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN 
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "moet alle 97 docenten return" {
+            $alle_docenten = Get-UntisDocenten
+            $alle_docenten.Count | Should -Be 97
+        }
+    }
+    Context "Demo GPN files vergelijken met export-data periode 1" {
+        $export_data_folders = @(
+            @{Path = "./demo-files/testdata_be_gy1_Hantal"},
+            @{Path = "./demo-files/testdata_be_uv1_Nijverheidsschool"}
+        )
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_gy1_Hantal.GPN
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "moet elke docent returnen die ook in '<Path>' voor periode 1 aanwezig is" -TestCases $export_data_folders {
+            param($Path)
+            $expected_teachers = Get-UntisDataFromExport -Path $Path -Type Teachers
+            $alle_docenten = Get-UntisDocenten #-Periode 1 #TODO
+            $expected_teachers | Foreach-Object {
+                $alle_docenten.afkorting | Should -Contain $_.afkorting
+            }
+            $alle_docenten | Foreach-Object {
+                $expected_teachers.afkorting | Should -Contain $_.afkorting
+            }
+        }
+    }
+}
+
+Describe 'Get-UntisActiviteiten' {
+    Context "Demo GPN file [be_uv1_Nijverheidsschool.gpn]" {
+        BeforeAll {
+            Open-UntisGpnFile -Path ./demo-files/be_uv1_Nijverheidsschool.gpn
+        }
+        AfterAll {
+            Close-UntisGpnFile
+        }
+        It "geeft het juiste aantal activiteiten terug" {
+            $expected = Get-UntisActiviteiten
+            $expected | Should -HaveCount 104
+        }
+        It "geeft de juiste values voor alle properties van de eerste activiteit" {
+            $expected = Get-UntisActiviteiten | Select-Object -First 1
+            $expected.nummer | Should -Be 179
+            $expected.startdatum | Should -BeOfType System.DateTime
+            $expected.einddatum | Should -BeOfType System.DateTime
+            $expected.startdatum | Should -Be ([datetime]'2010-05-07')
+            $expected.einddatum | Should -Be ([datetime]'2010-05-07')
+            $expected.reden | Should -Be "SB"
+            $expected.tekst | Should -Be "Moskee Genk+Zutendaal"
+            $expected.startuur | Should -Be 1845
+            $expected.einduur | Should -Be 9
+            $expected.onderwerp | Should -Be ''
+            $expected.klassen | Should -Be @('B2BA','B2BB','B2BC')
+            $expected.leerkrachten | Should -Be @('VLOB','Van6','VGIE','Geen','va')
+            $expected.absent_ids | Should -Be @(2814,2813,2812,2815,2816,2817,2818,2819)
+        }
+    }
+}
+
